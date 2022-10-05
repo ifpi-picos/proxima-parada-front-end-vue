0c46c31471e190f4a00fd08cb00c4999ef9f017d
edivan <edivansantos1478@gmail.com>
2022-10-05 19:20:02 -0300

    finalizando a centralização da parte de login

--- src/components/telaLogin.vue
+++ src/components/telaLogin.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="login">
-        <h1>Faça o login</h1>
-        <small>E-mail</small> <br>
-        <input type="text" placeholder="joãosilva@gmail.com">
-        <br>
-        <small>Senha</small> <br>
-        <input type="password" placeholder="Digite sua senha">
-        <br>
-        <button class="btn-login">Entrar</button> <br>
-        <button class="btn-cadastrar">Entrar</button>
+        <div class="box">
+            <h1>Faça o login</h1>
+            <small>E-mail</small> <br>
+            <input type="text" placeholder="joãosilva@gmail.com">
+            <br>
+            <small>Senha</small> <br>
+            <input type="password" placeholder="Digite sua senha">
+            <br>
+            <br>
+            
+            <button class="btn-login">Entrar</button> <br><br>
+            <button class="btn-cadastrar">Entrar</button>
 
-        <p> <a href="#" target="_blank">Você não possui conta </a> </p>
+            <p> <a href="#" target="_blank">Você não possui conta </a> </p>
+
+        </div>
+       
 
     </div>
 </template>
@@ -23,14 +29,26 @@
 
  <style>
     .login {
-        background:#f2f2f2;
+        background:#d3d0d0;
         text-align: center;
-    
-    }
-    
-    button{
-        background-color:#027bff;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center
 
+    }
+    .box{
+        width: 95%;
+        height: 95%;
+        background: #fff;
+    }
+   
+
+    .btn-login{
+        background-color:#027bff;
     }
     .btn-cadastrar{
         background-color: cadetblue;
