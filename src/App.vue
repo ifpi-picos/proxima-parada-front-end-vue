244b4b8ae0046790980ffef69cb473eeffba5616
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-05 14:06:03 -0300

    subindo alteraçoes

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<telaLogin/>
+<forms-usuario/>
+
+</template>
+
+<script>
+
+import telaLogin from './components/telaLogin.vue'
+import FormsUsuario from './components/formsUsuario.vue'
+
+    export default{
+        name:'App',
+        components:{
+            telaLogin,
+            FormsUsuario,
+                
+        }
+
+        
+    }
+</script>
+
+<style scoped>
+    
+
+
+</style>
---
+++ src/components/telaLogin.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+         <h1>Faça seu login aqui</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eaque aut voluptates totam, saepe voluptas alias labore architecto molestias vitae vel aspernatur unde possimus nemo. Ducimus optio aut illo quam?</p>
+
+    </div>
+   
+</template>
+
+<script>
+    export default {
+        name:"telaLogin"
+    }   
+</script>
+
+ <style scoped>
+    .container {
+        background:#f2f2f2;
+        width: 100%;
+        height: 100%;
+        border: 5px solid rgba(0,0,0,0.5);
+    }
+    
+ </style>
