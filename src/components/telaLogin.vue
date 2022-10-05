b48efac3ed624bbdfac56accd9ed4cbc9a596206
edivan <edivansantos1478@gmail.com>
2022-10-05 16:32:57 -0300

    teste

--- src/components/telaLogin.vue
+++ src/components/telaLogin.vue
@@ -1,21 +1,43 @@
 <template>
-    <h1>Faça o login</h1>
+    
     <div class="login">
-        <h1>Login</h1>
+        <h1>Faça o login</h1>
+        
+        <label >Email</label><br>
+            <input type="text" placeholder="Email">
+        <label for="">Senha
+            <a href="#" target="_blank">Esqueceu sua senha </a>
+        </label>
+        <input  type="password" placeholder="Senha"> <br>
+        <button type="button"   class=""> Entrar</button><br>
+        <button type="button"   class="butaoCriarConta">Criar Conta</button>
+
+
 
     </div>
 </template>
 
-<script>
+<script scoped>
     export default {
         name:"telaLogin"
     }   
 </script>
 
- <style scoped>
+ <style>
     .container {
         background:#f2f2f2;
     
     }
+    small{
+        color: blue;
+    }
+    button{
+        background-color:#027bff;
+
+    }
+    .butaoCriarConta{
+        background-color: cadetblue;
+    }
+
     
  </style>
