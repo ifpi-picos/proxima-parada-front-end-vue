8a383c2876311bd7fab71f64ba2c38ea8c326d77
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-10 09:11:18 -0300

    importando o bootstrap no arquivo main

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+Vue.config.productionTip = false
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
