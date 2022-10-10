16b90caeda8b36180c744a8dc564125d8a6c85d8
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-10 09:42:36 -0300

    criando a pagina de login da aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-
- 
+    <div>
+      <tela-login/>
+    </div>
 </template>
+
+<script>
+import TelaLogin from './components/telaLogin.vue'
+export default {
+  name:'App',
+  components:{
+    TelaLogin
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/telaLogin.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="vue-tempalte">
+        <form class="inner-block">
+            <h3>Faça o login</h3>
+            <div class="form-group">
+                <label>E-mail</label>
+                <input type="email" class="form-control form-control-lg"/>
+
+            </div>
+
+            <div class="form-group">
+                <label>Senha</label>
+                <input type="password" class="form-control form-control-lg"/>
+
+            </div>
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Entrar</button>
+
+            <p class="forgot-password text-right mt-2 mb-4">
+                <router-link to="/forgot-password">Esqueceu sua senha ?</router-link>
+            </p>
+
+           
+        </form>
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name:'TelaLogin',
+        data() {
+            return {}
+        }
+    }
+</script>
