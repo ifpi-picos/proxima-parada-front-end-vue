a299ac2df7008af7e27016a29c0022120ba5f3da
Rafhael0069 <capic.2018tads0069@aluno.ifpi.edu.br>
2022-10-11 10:55:35 -0300

    Adicionando elememtos da view principal para a autenticação

---
+++ src/views/AuthView.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+                <v-card>
+                    <v-tabs fixed-tabs background-color="primary">
+                        <v-tab>
+                            Entrar
+                        </v-tab>
+                        <v-tab>
+                            Cadastre-se
+                        </v-tab>
+                        <v-tab-item>
+                            <template>
+                                <sign-in />
+                            </template>
+                        </v-tab-item>
+                        <v-tab-item>
+                            <template>
+                                <sign-up />
+                            </template>
+                        </v-tab-item>
+                    </v-tabs>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+  
+<script>
+import SignIn from '../components/SignIn.vue'
+import SignUp from '../components/SignUp.vue'
+
+export default {
+    name: 'Auth-main',
+    components: {
+        // eslint-disable-next-line vue/no-unused-components
+        SignIn,
+        SignUp
+    },
+    data() {
+    },
+}
+</script>
