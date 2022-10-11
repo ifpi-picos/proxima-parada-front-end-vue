fd23d1342addb3aab9d357099c8f2c716658237d
Rafhael0069 <capic.2018tads0069@aluno.ifpi.edu.br>
2022-10-11 10:53:44 -0300

    Adicionando elementos dos componentes SignIn e SignUo

---
+++ src/components/SignIn.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-card class="page">
+       <v-card-text>
+          <v-form>
+             <v-text-field prepend-icon="mail_outline" name="login" label="E-mail" type="text">
+             </v-text-field>
+             <v-text-field id="password" prepend-icon="password" name="password" label="Senha" type="password">
+             </v-text-field>
+          </v-form>
+       </v-card-text>
+       <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn block color="primary" elevation="6" to="/">Entrar</v-btn>
+       </v-card-actions>
+    </v-card>
+ </template>
+  
+ <script>
+ export default {
+    name: "SignIn",
+    data() {
+    },
+ }
+ </script>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-card class="page">
+        <v-card-text>
+            <v-form>
+                <v-text-field prepend-icon="perm_identity" name="name" label="Nome Completo" type="text">
+                </v-text-field>
+                <v-text-field prepend-icon="work_outline" name="occupation" label="Ocupação no IFPI" type="text">
+                </v-text-field>
+                <v-text-field prepend-icon="perm_device_information" name="phone" label="Número de Whatsapp"
+                    type="phone">
+                </v-text-field>
+                <v-text-field prepend-icon="mail_outline" name="login" label="E-mail" type="text">
+                </v-text-field>
+                <v-text-field prepend-icon="password" name="password" label="Senha" type="password">
+                </v-text-field>
+                <v-text-field prepend-icon="password" name="password-confirm" label="Comfirmar senha" type="password">
+                </v-text-field>
+            </v-form>
+        </v-card-text>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn block color="primary" elevation="6">Cadastre-se</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+export default {
+    name: "SignUp",
+    data() {
+    },
+}
+</script>
