158bb5c2bb3ffcc4b5208a5d5ccb7e67163a46dc
Rafhael0069 <capic.2018tads0069@aluno.ifpi.edu.br>
2022-10-12 13:34:25 -0300

    Adicionando metodos de retorno visual para validações de dados de entrada e form

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -4,10 +4,11 @@
          <v-img :src="require('../assets/logo.png')" class="my-3" contain height="260" />
          <v-form @submit.prevent="auth">
             <v-text-field prepend-icon="mail_outline" name="userEmail" label="E-mail" type="text"
-               v-model="userAuth.email">
+               :rules="[rules.required, rules.email]" v-model="userAuth.email">
             </v-text-field>
-            <v-text-field prepend-icon="password" name="encryptedPassword" label="Senha" type="password"
-               v-model="userAuth.password">
+            <v-text-field prepend-icon="password" name="encryptedPassword" label="Senha"
+               :rules="[rules.required, rules.min ]" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+               :type="show1 ? 'text' : 'password'" @click:append="show1 = !show1" v-model="userAuth.password">
             </v-text-field>
             <v-btn block color="primary" elevation="6" type="submit">Entrar</v-btn>
          </v-form>
@@ -25,6 +26,15 @@ export default {
          userAuth: {
             email: "",
             password: ""
+         },
+         show1: false,
+         rules: {
+            required: value => !!value || 'Obrigatório.',
+            min: value => value.length >= 6 || 'Mínimo 6 caracteres',
+            email: value => {
+               const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+               return pattern.test(value) || 'E-mail inválido.'
+            },
          }
       }
    },
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,29 +3,29 @@
         <v-card-text>
             <v-form @submit.prevent="createUser">
                 <v-text-field prepend-icon="perm_identity" name="name" label="Nome Completo" type="text"
-                    v-model="userData.name">
+                    :rules="[rules.required]" password v-model="userData.name">
                 </v-text-field>
                 <v-text-field prepend-icon="work_outline" name="occupation" label="Ocupação no IFPI" type="text"
-                    v-model="userData.occupation">
+                    :rules="[rules.required]" v-model="userData.occupation">
                 </v-text-field>
                 <v-text-field prepend-icon="perm_device_information" name="phone_number" label="Número de Whatsapp"
-                    type="phone" v-model="userData.phone_number">
+                    :rules="[rules.required]" type="phone" v-model="userData.phone_number">
                 </v-text-field>
                 <v-text-field prepend-icon="mail_outline" name="email" label="E-mail" type="text"
-                    v-model="userData.email">
+                    :rules="[rules.required, rules.email]" v-model="userData.email">
                 </v-text-field>
-                <v-text-field prepend-icon="password" name="password" label="Senha" type="password"
-                    v-model="userData.password">
+                <v-text-field prepend-icon="password" name="password" label="Senha"
+                    :rules="[rules.required, rules.min ]" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show1 ? 'text' : 'password'" @click:append="show1 = !show1" v-model="userData.password">
                 </v-text-field>
-                <v-text-field prepend-icon="password" name="password-confirm" label="Comfirmar senha" type="password">
+                <v-text-field prepend-icon="password" name="password_confirm" label="Comfirmar senha"
+                    :rules="[rules.required, rules.min, rules.matchPassword]"
+                    :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'" :type="show2 ? 'text' : 'password'"
+                    @click:append="show2 = !show2" v-model="password_confirm">
                 </v-text-field>
                 <v-btn block color="primary" elevation="6" type="submit">Cadastre-se</v-btn>
             </v-form>
         </v-card-text>
-        <!-- <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn block color="primary" elevation="6">Cadastre-se</v-btn>
-        </v-card-actions> -->
     </v-card>
 </template>
 <script>
@@ -40,6 +40,18 @@ export default {
                 phone_number: "",
                 email: "",
                 password: ""
+            },
+            password_confirm: "",
+            show1: false,
+            show2: false,
+            rules: {
+                required: value => !!value || 'Obrigatório.',
+                min: value => value.length >= 6 || 'Min 6 characters',
+                email: value => {
+                    const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    return pattern.test(value) || 'E-mail inválido.'
+                },
+                matchPassword: () => this.userData.password === this.password_confirm || `Senhas diferentes`,
             }
         }
     },
