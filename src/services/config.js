8d0516abacd569b52553dcd35a70475b5c9ee03e
Rafhael0069 <capic.2018tads0069@aluno.ifpi.edu.br>
2022-10-18 09:31:40 -0300

    Adicionando erros padrões, alterando o tipo de ocupação e adicionando mascara ou numero de telefone

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const http = axios.create({
-  /* baseURL: "https://proxima-parada.onrender.com/api/", */
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://proxima-parada.onrender.com/api/",
+  /* baseURL: "http://localhost:3000/api/", */
 });
