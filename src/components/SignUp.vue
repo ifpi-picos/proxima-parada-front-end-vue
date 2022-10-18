dcf850088ed5495c8b39eb776f30180a3821ff23
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-18 09:00:10 -0300

    criando a estrutura da tela de perfil e corrigindo o tipo de input do campo de whatsap

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -12,7 +12,7 @@
                     :rules="[rules.required]" v-model="userData.occupation">
                 </v-text-field>
                 <v-text-field prepend-icon="perm_device_information" name="phone_number" label="Número de Whatsapp"
-                    :rules="[rules.required]" type="phone" v-model="userData.phone_number">
+                    :rules="[rules.required]" type="number" v-model="userData.phone_number">
                 </v-text-field>
                 <v-text-field prepend-icon="mail_outline" name="email" label="E-mail" type="text"
                     :rules="[rules.required, rules.email]" v-model="userData.email">
