edb1e77428367a69c2b5bf70cdc568816b80b713
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-26 09:30:21 -0300

    colocando a tela de perfil centralizada

--- src/App.vue
+++ src/App.vue
@@ -89,19 +89,3 @@ export default {
 <style>
 </style>
 
-<!-- <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
-</template>
-
-<script>
-
-export default {
-  name: 'App',
-  data: () => ({
-  }),
-};
-</script> -->
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,23 +1,25 @@
 <template>
 
-    <v-card>
+    <v-card class="container ">
 
-        <v-card-text>
+        <v-card-text class="box">
             <div class="imagem">
                 <v-img src="../../src/assets/stite.jpg" class="img" />
                 <figcaption> {{userData.name}}</figcaption>
             </div>
             <div class="status">
-                <h3 >Ocupação no Campus: {{userData.occupation}} </h3>
-                <h3>E-mail: {{userData.email}}</h3>
-                <h3>status: {{userData.status}}</h3>
-            </div> <br>
-
-            <v-form  v-model="valid">
+                <p >Ocupação no Campus:  <strong>{{userData.occupation}}</strong> </p>
+                <p>E-mail: <strong>{{userData.email}}</strong></p>
+                <p>status: <strong>{{userData.status}}</strong> </p> <br>
+                
+                <v-form  v-model="valid">
                 <v-row>
-                    <v-btn depressed color="primary" type="submit" width="100vw" font-size="45px"> Editar Perfil</v-btn>
+                    <v-btn depressed color="primary" type="submit" width="90%" font-size="45px"> Editar Perfil</v-btn>
                 </v-row>
             </v-form>
+            </div> <br>
+
+         
 
         </v-card-text>
     </v-card>
@@ -39,19 +41,35 @@ export default {
 </script>
 
 <style scoped>
-.img {
-    border-radius: 50%;
-    border: 2px solid rgba(0, 0, 0, 0.575);
-    width: 150px;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
+    .img {
+        border-radius: 50%;
+        border: 2px solid rgba(0, 0, 0, 0.575);
+        width: 150px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
 
-}
-figcaption{
-    text-align: center;
-    font-size: 18px;
-    padding: 5px;
-}
-
+    }
+    figcaption{
+        text-align: center;
+        font-size: 18px;
+        padding: 15px;
+    }
+    .container {
+        width: 400px;
+        height: 500px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+    .box {
+        width: 300px;
+        height: 400px;
+        background: #fff;
+    }
+    p{
+    
+        font-size: 18px;
+    }
 </style>
