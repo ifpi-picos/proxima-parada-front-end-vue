1c035d0348b89797cea3a25cd535c9f3fa48f90f
EdivanSantos <capic.20191tads0326@aluno.ifpi.edu.br.edu>
2022-11-08 09:19:42 -0300

    refatorando codigo para ficar mais organizado

--- src/pages/home/Cadastrar-Carona.vue
+++ src/pages/home/Cadastrar-Carona.vue
@@ -1,114 +1,69 @@
- <template> 
-    <v-row justify="center">
-      <v-col cols="12" sm="10" md="8" lg="6">
-        <v-card ref="form">
-          
-          <v-card-text>
-            <h1> Bem-Vindo Ao cadastro de Caronas </h1>
-            <br><br>
-            <h2>Informações Pessoais</h2>
+<template> 
+  <v-row justify="center">
+    <v-col cols="12" sm="10" md="8" lg="6">
+      <v-card ref="form">
 
-            <v-text-field placeholder="João da silva" ref="name" v-model="name" :rules="[() => !!name || 'Esse Campo e obrigatorio']"
-              :error-messages="errorMessages" label="Nome Completo" required/>
+        <v-card-text>
+          <h1> Bem-Vindo Ao cadastro de Caronas </h1>
+          <br><br>
+          <h2>Informações Pessoais</h2>
 
-              <v-text-field placeholder="(XX)XXXXX-XXXX" ref="number-phone" label="Numero para contato" type="number" :rules="[() => !!name || 'Esse Campo e obrigatorio']"/>
+          <v-text-field placeholder="João da silva" ref="name" v-model="name" 
+          :rules="[() => !!name || 'Esse      Campo  e obrigatorio']"
+          :error-messages="errorMessages" label="Nome Completo" required/>
+
+          <v-text-field placeholder="(XX)XXXXX-XXXX" ref="number-phone" label="Numero para contato" type="number" 
+          :rules="[() => !!name || 'Esse Campo e obrigatorio']"/>
+        </v-card-text>
+
+        <v-card-text>   
+          <h2>De onde estou Saindo</h2>
             
-              <h2>De onde estou Saindo</h2>
-            
-            <v-text-field ref="address" v-model="address"  :rules="[
-                () => !!address || 'Esse Campo e obrigatorio',]"
-                label="Informe o Bairro" placeholder="Bairro Junco "/>
+          <v-text-field ref="address" v-model="address"  :rules="[() => !!address || 'Esse Campo e obrigatorio',]"
+          label="Nome do Bairro" placeholder="Bairro Junco "/>
 
-                <v-text-field ref="address"  :rules="[ () => !!address || 'Esse Campo e obrigatorio',]"
-                label="Informe a Rua " placeholder="Rua do pcc "/>
+          <v-text-field ref="address"  :rules="[ () => !!address || 'Esse Campo e obrigatorio',]"
+          label="Nome da Rua " placeholder="Rua do pcc "/>
 
-                <v-text-field label="Número da Casa " placeholder="288" type="number"/>
+          <v-text-field label="Número da Casa " placeholder="288" type="number"/>
 
-                <v-text-field v-model="time" 
-                label="Informe Dia Mes e Ano " placeholder=" 01/02/2022" type="date"/>
+          <v-text-field v-model="time" 
+          label="Digite o Dia Mes e Ano " placeholder=" 01/02/2022" type="date"/>
 
-                <v-text-field v-model="time" 
-                label="Informe a Hora exata" placeholder=" 01/02/2022" type="time"/>
-                <h2>Pra onde vou</h2>
+          <v-text-field v-model="time" 
+          label="Digite a hora da Saída" placeholder=" 12:00" type="time"/>
+        </v-card-text> 
 
-                <v-text-field ref="address"  
-                label="Informe o Bairro " placeholder="Rua do pcc "/>
+        <v-card-text>
+          <h2>Pra onde vou</h2>
+          <v-text-field ref="address"  
+          label="Digite do Bairro " placeholder="Rua do pcc "/>
 
-                <v-text-field ref="address"  
-                label="Informe o nome da rua " placeholder="Rua do pcc "/>
+          <v-text-field  
+          label="Digite o nome da rua " placeholder="Rua do pcc "/>
 
-                <v-text-field ref="address"  
-                label="Informe o numero da casa" placeholder="Rua do pcc "/>
+          <v-text-field type="number"  
+          label="Digite o numero da casa" placeholder="Rua do pcc "/>
 
-                
+        </v-card-text>
 
-            <v-text-field
-              ref="state"
-              v-model="state"
-              :rules="[() => !!state || 'This field is required']"
-              label="State/Province/Region"
-              required
-              placeholder="TX"
-            ></v-text-field>
-            <v-text-field
-              ref="zip"
-              v-model="zip"
-              :rules="[() => !!zip || 'This field is required']"
-              label="ZIP / Postal Code"
-              required
-              placeholder="79938"
-            ></v-text-field>
-            <v-autocomplete
-              ref="country"
-              v-model="country"
-              :rules="[() => !!country || 'This field is required']"
-              :items="countries"
-              label="Country"
-              placeholder="Select..."
-              required
-            ></v-autocomplete>
-          </v-card-text>
-          
-          <v-divider class="mt-12"></v-divider>
+        <v-card-text>
+
+
+        </v-card-text>
+          <v-divider class="mt-12"/>
           <v-card-actions>
-            <v-btn text>
-              Cancel
-            </v-btn>
+            <v-btn text> Cancelar </v-btn>
+
             <v-spacer></v-spacer>
-            <v-slide-x-reverse-transition>
-              <v-tooltip
-                v-if="formHasErrors"
-                left
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    icon
-                    class="my-0"
-                    v-bind="attrs"
-                    @click="resetForm"
-                    v-on="on"
-                  >
-                    <v-icon>mdi-refresh</v-icon>
-                  </v-btn>
-                </template>
-                <span>Refresh form</span>
-              </v-tooltip>
-            </v-slide-x-reverse-transition>
-            <v-btn
-              color="primary"
-              text
-              @click="submit"
-            >
-              Submit
-            </v-btn>
+            <v-btn color="blue" text @click="submit"> Salvar Carona </v-btn>
           </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
 
- </template>
-
- <script>
+<script>
  export default {
  data: () => ({
     errorMessages: '',
@@ -142,23 +97,8 @@
   },
 
   methods: {
-    addressCheck () {
-      this.errorMessages = this.address && !this.name
-        ? `Hey! I'm required`
-        : ''
-
-      return true
-    },
-    resetForm () {
-      this.errorMessages = []
-      this.formHasErrors = false
-
-      Object.keys(this.form).forEach(f => {
-        this.$refs[f].reset()
-      })
-    },
     submit () {
-      this.formHasErrors = false
+      this.formHasErrors = true
 
       Object.keys(this.form).forEach(f => {
         if (!this.form[f]) this.formHasErrors = true
@@ -167,13 +107,9 @@
       })
     },
   },
-}
-
-    
+} 
 </script>
  
- 
-
  <!-- <template>
    <v-card >
         <div class="title">
