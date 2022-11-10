7408a495948a357735b66e56e2e5d8ee03c137c9
EdivanSanto5 <cabic.20191tads0326@aluno.ifpi.edu.br>
2022-11-10 16:43:10 -0300

    finalizando fatoramento

--- src/pages/home/Cadastrar-Carona.vue
+++ src/pages/home/Cadastrar-Carona.vue
@@ -8,68 +8,63 @@
 
       <v-col cols="12" xm="8" sm="8" md="8" lg="10">
         <v-alert :value="alertError" color="red" elevation="3" outlined :type="alertType">{{ alertMessage }}</v-alert>
-        <v-card
-          max-width="450px"
-          min-width="400px"
-          class="mx-auto bg pt-10"
-          elevation="2"
-        >
-        <div >
-        <v-card>
+        <v-card max-width="450px" min-width="400px" class="mx-auto bg pt-10" elevation="2">
+        <div>
+          <v-card>
             <v-container>
-                <v-row >
-                    <v-col cols="6" >
-                        <v-card color="#385F73" dark class="cards" >
-                            <v-card-title>
-                                <h2>Nome do Usuario</h2>
-                                </v-card-title>
-                                <p>Status:Professor</p>
-                                <p>status da Carona:</p>
-                                <p>Quantidade de Vagas</p>
-                                <p>Data:12:00 as 14/11/2022</p>
-                                <p>Quantidade de Vagas : 3</p>
-                            </v-card>
-                    </v-col>
-                    <v-col cols="6">
-                        <v-card color="#385F73" dark class="cards" >
-                            <v-card-title>
-                                <h2>Nome do Usuario</h2>
-                                </v-card-title>
-                                <p>Status:Professor</p>
-                                <p>status da Carona:</p>
-                                <p>Quantidade de Vagas</p>
-                                <p>Data:12:00 as 14/11/2022</p>
-                                <p>Quantidade de Vagas : 2</p>
-                        </v-card>
-                    </v-col>
-                    <v-col cols="6">
-                        <v-card color="#385F73" dark class="cards" >
-                            <v-card-title>
-                                <h2>Nome do Usuario</h2>
-                                </v-card-title>
-                                <p>Status:Professor</p>
-                                <p>status da Carona:</p>
-                                <p>Quantidade de Vagas</p>
-                                <p>Data:12:00 as 14/11/2022</p>
-                                <p>Quantidade de Vagas : 1</p>
-                        </v-card>
-                    </v-col>
-                    <v-col cols="6">
-                        <v-card color="#385F73" dark class="cards" >
-                            <v-card-title>
-                                <h2>Nome do Usuario</h2>
-                                </v-card-title>
-                                <p>Status:Professor</p>
-                                <p>status da Carona:</p>
-                                <p>Quantidade de Vagas</p>
-                                <p>Data:12:00 as 14/11/2022</p>
-                                <p>Quantidade de Vagas : 4</p>
-                        </v-card>
-                    </v-col>
-                </v-row>
-             </v-container>
-           </v-card>   
-         </div>
+              <v-row>
+                <v-col cols="6" >
+                  <v-card color="#385F73" dark class="cards" >
+                    <v-card-title>
+                      <h2>Nome do Usuario</h2>
+                    </v-card-title>
+                        <p>Status:Professor</p>
+                        <p>status da Carona:</p>
+                        <p>Quantidade de Vagas</p>
+                        <p>Data:12:00 as 14/11/2022</p>
+                        <p>Quantidade de Vagas : 3</p>
+                      </v-card>
+                      </v-col>
+                      <v-col cols="6">
+                          <v-card color="#385F73" dark class="cards" >
+                              <v-card-title>
+                                  <h2>Nome do Usuario</h2>
+                                  </v-card-title>
+                                  <p>Status:Professor</p>
+                                  <p>status da Carona:</p>
+                                  <p>Quantidade de Vagas</p>
+                                  <p>Data:12:00 as 14/11/2022</p>
+                                  <p>Quantidade de Vagas : 2</p>
+                          </v-card>
+                      </v-col>
+                      <v-col cols="6">
+                          <v-card color="#385F73" dark class="cards" >
+                              <v-card-title>
+                                  <h2>Nome do Usuario</h2>
+                                  </v-card-title>
+                                  <p>Status:Professor</p>
+                                  <p>status da Carona:</p>
+                                  <p>Quantidade de Vagas</p>
+                                  <p>Data:12:00 as 14/11/2022</p>
+                                  <p>Quantidade de Vagas : 1</p>
+                          </v-card>
+                      </v-col>
+                      <v-col cols="6">
+                          <v-card color="#385F73" dark class="cards" >
+                              <v-card-title>
+                                  <h2>Nome do Usuario</h2>
+                            </v-card-title>
+                                  <p>Status:Professor</p>
+                                  <p>status da Carona:</p>
+                                  <p>Quantidade de Vagas</p>
+                                  <p>Data:12:00 as 14/11/2022</p>
+                                  <p>Quantidade de Vagas : 4</p>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>   
+        </div>
 
           
           <v-container>
